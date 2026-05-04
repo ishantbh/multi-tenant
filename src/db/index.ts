46a75aec41270ab14b10f31cec5852cf5c25f704
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-04 09:20:41 +0530

    update drizzle connection to be a singleton to avoid unnecessary instances with HMR

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,11 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 
-export const db = drizzle(process.env.DATABASE_URL!)
+const globalForDb = globalThis as unknown as {
+  db: ReturnType<typeof drizzle> | undefined
+}
+
+export const db = globalForDb.db ?? drizzle(process.env.DATABASE_URL!)
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForDb.db = db
+}
