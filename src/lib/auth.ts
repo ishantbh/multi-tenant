cf33ab2304e763497eeb1c0bde8fc294a6182ccc
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-04 13:25:50 +0530

    update auth config by passing auth schema to drizzle adapter in auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,5 @@
 import { db } from '@/db'
+import * as authSchema from '@/db/schema/auth'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { nextCookies } from 'better-auth/next-js'
@@ -6,6 +7,7 @@ import { nextCookies } from 'better-auth/next-js'
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
+    schema: authSchema,
   }),
   emailAndPassword: {
     enabled: true,
