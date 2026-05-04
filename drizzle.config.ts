60f92cb0c6877c236e5dc86f805700c2e581fb6a
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-04 12:42:38 +0530

    setup better-auth with auth schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import './envConfig'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
+  schema: './src/db/schema',
   out: './migrations',
   dialect: 'postgresql',
   dbCredentials: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,15 @@
-import { Button } from '@/components/ui/button'
-import { ThemeToggle } from '@/components/ui/theme/theme-toggle'
-import { db } from '@/db'
-import { todo } from '@/db/schema'
-import { revalidatePath } from 'next/cache'
-
-async function addTodo(formData: FormData) {
-  'use server'
-
-  const text = formData.get('text')
-
-  if (!text) {
-    return
-  }
-
-  const newTodo: typeof todo.$inferInsert = {
-    id: Math.floor(Math.random() * 10000),
-    text: text as string,
-  }
-
-  await db.insert(todo).values(newTodo)
-
-  revalidatePath('/')
-}
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export default async function HomePage() {
-  const todos = await db.select().from(todo)
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
 
-  return (
-    <div className='p-4 space-y-4'>
-      <h1>Home Page</h1>
-      <ThemeToggle />
-      <form action={addTodo}>
-        <div className='flex items-center gap-2'>
-          <input
-            id='text'
-            type='text'
-            name='text'
-            className='border px-2 py-1 rounded-md bg-muted'
-            placeholder='Add a todo'
-          />
-          <Button type='submit' variant='outline'>
-            Add
-          </Button>
-        </div>
-      </form>
-      {todos.length ? (
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}>{todo.text}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No todos yet</p>
-      )}
-    </div>
-  )
+  if (!session) {
+    redirect('/login')
+  }
+
+  return <div>Home Page</div>
 }
