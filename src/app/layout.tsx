14f2c4f612f0c3e8667a9a6faa7f29bc01a0987b
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-04 08:56:50 +0530

    add theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next'
 import { Geist_Mono, Inter } from 'next/font/google'
-import './globals.css'
+
 import { cn } from '@/lib/utils'
+
+import { ThemeProvider } from '@/components/ui/theme/theme-provider'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
@@ -24,13 +28,23 @@ export default function RootLayout({
   return (
     <html
       lang='en'
+      suppressHydrationWarning
       className={cn(
         'h-full antialiased font-sans',
         geistMono.variable,
         inter.variable,
       )}
     >
-      <body className='min-h-full flex flex-col'>{children}</body>
+      <body className='min-h-full flex flex-col'>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
