04527784c05f8f70ff33109a8c597abe1c5260f3
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-06 10:00:23 +0530

    add theme toggle to header

--- src/app/(protected)/_components/header.tsx
+++ src/app/(protected)/_components/header.tsx
@@ -1,12 +1,16 @@
 import { UserAvatar } from './user-avatar'
 import { OrganizationSwitcher } from './organization-switcher'
+import { ThemeToggle } from '@/components/ui/theme/theme-toggle'
 
 export async function Header() {
   return (
     <header className='p-4 border-b'>
       <div className='flex gap-2 items-center justify-between'>
         <OrganizationSwitcher />
-        <UserAvatar />
+        <div className='flex items-center gap-2'>
+          <UserAvatar />
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   )
