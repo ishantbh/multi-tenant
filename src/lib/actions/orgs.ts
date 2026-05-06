e5ecf12bc94092c16a4f88791f37269d49be97c6
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-06 10:04:08 +0530

    add server-action to create org

---
+++ src/lib/actions/orgs.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import { auth } from '@/lib/auth'
+import { CreateOrgInput, createOrgSchema } from '@/lib/validation/orgs'
+import { ActionResult } from '@/types'
+import { headers } from 'next/headers'
+
+export async function createOrgAction(
+  data: CreateOrgInput,
+): Promise<ActionResult> {
+  const parsed = createOrgSchema.safeParse(data)
+
+  if (!parsed.success) {
+    return { success: false, error: parsed.error.message }
+  }
+
+  const { name, slug } = parsed.data
+
+  try {
+    const reqHeaders = await headers()
+
+    await auth.api.createOrganization({
+      body: {
+        name,
+        slug,
+      },
+      headers: reqHeaders,
+    })
+
+    return { success: true }
+  } catch (err) {
+    return {
+      success: false,
+      error: err instanceof Error ? err.message : 'Error createing workspace',
+    }
+  }
+}
